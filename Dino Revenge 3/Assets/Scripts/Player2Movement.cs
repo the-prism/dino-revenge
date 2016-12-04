ba4f871e11b3416733f00540daa36c167b3a8cd1
DeadmanM <deslauriers.maxime@gmail.com>
2016-12-04 16:55:30 -0500

    Players control implementation

--- Dino Revenge 3/Assets/Scripts/Player2Movement.cs
+++ Dino Revenge 3/Assets/Scripts/Player2Movement.cs
@@ -1,66 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerMovement : MonoBehaviour {
-
+public class Player2Movement : MonoBehaviour {
+	
 	public float speed = 6f;
-
+	
 	Vector3 movement;
 	Animator anim;
 	Rigidbody playerRigidBody;
 	int floorMask;
 	float camRayLength = 100f;
-
+	
 	void Awake()
 	{
 		floorMask = LayerMask.GetMask ("Floor");
 		anim = GetComponent<Animator> ();
 		playerRigidBody = GetComponent<Rigidbody> ();
 	}
-
+	
 	void FixedUpdate()
 	{
 		float h = 0;
 		float v = 0;
-		if (Input.GetKey ("right"))
+		if (Input.GetKey ("a"))
 			h += 1;
-		if (Input.GetKey ("left"))
+		if (Input.GetKey ("d"))
 			h += -1;
-		if (Input.GetKey ("up"))
+		if (Input.GetKey ("s"))
 			v += 1;
-		if (Input.GetKey ("down"))
+		if (Input.GetKey ("w"))
 			v += -1;
-
+		
 		Move (h, v);
 		Turning ();
 		//Animating (h, v);
 	}
-
+	
 	void Move(float h, float v)
 	{
 		movement.Set (h, 0f, v);
-
+		
 		movement = movement.normalized * speed * Time.deltaTime;
-
+		
 		playerRigidBody.MovePosition (transform.position + movement);
 	}
-
+	
 	void Turning()
 	{
 		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
-
+		
 		RaycastHit floorHit;
-
+		
 		if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)) 
 		{
 			Vector3 playerToMouse = floorHit.point - transform.position;
 			playerToMouse.y = 0f;
-
+			
 			Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
 			playerRigidBody.MoveRotation(newRotation);
 		}
 	}
-
+	
 	void Animating(float h, float v)
 	{
 		bool walking = h != 0f || v != 0f;
