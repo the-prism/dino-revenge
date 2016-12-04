3a5d4545b71afc6d2d44c80789f4a9d3d50ffda5
DeadmanM <deslauriers.maxime@gmail.com>
2016-12-04 15:48:00 -0500

    Split screen mode

---
+++ Dino Revenge 3/Assets/Scripts/DestroyCubeController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyCubeController : MonoBehaviour {
+	public GameObject remains;
+	void Update () 
+	{
+		if (Input.GetKey ("space")) 
+		{
+			Instantiate(remains, transform.position, transform.rotation);
+			Destroy(gameObject);
+		}
+	}
+}
