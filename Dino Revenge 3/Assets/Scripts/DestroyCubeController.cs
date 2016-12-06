3b175159abc6f9ae5d955b56b90a35388dae054f
DeadmanM <deslauriers.maxime@gmail.com>
2016-12-06 13:33:46 -0500

    TODO + updated player controls

--- Dino Revenge 3/Assets/Scripts/DestroyCubeController.cs
+++ Dino Revenge 3/Assets/Scripts/DestroyCubeController.cs
@@ -3,12 +3,15 @@ using System.Collections;
 
 public class DestroyCubeController : MonoBehaviour {
 	public GameObject remains;
+	public float playerId;
 	void Update () 
 	{
-		if (Input.GetKey ("space")) 
+		if ( (playerId == 1 && Input.GetKey ("e")) || (playerId == 2 && Input.GetKey ("o")) )
 		{
-			GameObject t = (GameObject)Instantiate(remains, transform.position, transform.rotation);
-			Destroy(gameObject);
+			GameObject t = (GameObject)Instantiate (remains, transform.position, transform.rotation);
+			Destroy (gameObject);
+			new WaitForSeconds (10);
+			Destroy (t, 1.0f);
 		}
 	}
 }
