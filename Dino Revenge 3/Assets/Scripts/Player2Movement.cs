2ef269e66d4257f7aa33b791940589e2f67519dc
the-prism <t.castonguay.gagnon@gmail.com>
2016-12-13 10:10:59 -0500

    Anim first draft

--- Dino Revenge 3/Assets/Scripts/Player2Movement.cs
+++ Dino Revenge 3/Assets/Scripts/Player2Movement.cs
@@ -1,89 +1,124 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player2Movement : MonoBehaviour {
-	
-	public float speed = 6f;
-	public GameObject cube;
-	
-	Vector3 movement;
-	Animator anim;
-	Rigidbody playerRigidBody;
-	float TurnInputValue;
-	float TurnSpeed = 180f;
-	string TurnAxisName;
-	GameObject t;
+public class Player2Movement : MonoBehaviour
+{
+
+    public float speed = 6f;
+    public GameObject cube;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidBody;
+    float TurnInputValue;
+    float TurnSpeed = 180f;
+    string TurnAxisName;
+    GameObject t;
     bool canPlaceBomb = true;
-	
-	
-	void OnEnable()
-	{
-		//TurnInputValue = 0f;
-		//playerRigidBody.isKinematic = false;
-	}
-	
-	private void OnDisable ()
-	{
-		// When the tank is turned off, set it to kinematic so it stops moving.
-		//playerRigidBody.isKinematic = true;
-	}
-	
-	void Awake()
-	{
-		anim = GetComponent<Animator> ();
-		playerRigidBody = GetComponent<Rigidbody> ();
-	}
-	
-	void Start()
-	{
-		TurnAxisName = "Horizontal";
-	}
-	
-	void Update()
-	{
-		if(Input.GetKey("escape"))
-		{
-			UnityEditor.EditorApplication.isPlaying = false;
-			Application.Quit ();
-		}
-		if(transform.position.y < -50 || transform.position.x > 500 || transform.position.z > 500)
-		{
-			MainMenuController.player = 1;
-			Application.LoadLevel("Gameover");
-		}
-		float horizontalAxis = 0;
-		float verticalAxis = 0;
-		if (Input.GetKey ("j"))
-		{
-			horizontalAxis += 1;
-		}
-		if (Input.GetKey ("l")) 
-		{
-			horizontalAxis += -1;
-		}
-		if (Input.GetKey ("k")) 
-		{
-			verticalAxis += 1;
-		}
-		if (Input.GetKey ("i")) 
-		{
-			verticalAxis += -1;
-		}
-		if (Input.GetKey ("u")) 
-		{
-			float x = transform.position.x;
-			float y = transform.position.y;
-			float z = transform.position.z;
+
+
+    void OnEnable()
+    {
+        //TurnInputValue = 0f;
+        //playerRigidBody.isKinematic = false;
+    }
+
+    private void OnDisable()
+    {
+        // When the tank is turned off, set it to kinematic so it stops moving.
+        //playerRigidBody.isKinematic = true;
+    }
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        TurnAxisName = "Horizontal";
+    }
+
+    void Update()
+    {
+        if ( Input.GetKey( "escape" ) )
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+            Application.Quit();
+        }
+        if ( transform.position.y < -50 || transform.position.x > 500 || transform.position.z > 500 )
+        {
+            MainMenuController.player = 1;
+            Application.LoadLevel( "Gameover" );
+        }
+        float horizontalAxis = 0;
+        float verticalAxis = 0;
+
+        if ( Input.GetKey( "j" ) )
+        {
+            anim.SetBool( "isWalk", true );
+            horizontalAxis += 1;
+        }
+        else if ( Input.GetKeyUp( "j" ) )
+        {
+            anim.SetBool( "isWalk", false );
+        }
+
+        if ( Input.GetKey( "l" ) )
+        {
+            anim.SetBool( "isWalk", true );
+            horizontalAxis += -1;
+        }
+        else if ( Input.GetKeyUp( "l" ) )
+        {
+            anim.SetBool( "isWalk", false );
+        }
+
+        if ( Input.GetKey( "k" ) )
+        {
+            anim.SetBool( "isWalk", true );
+            verticalAxis += 1;
+        }
+        else if ( Input.GetKeyUp( "k" ) )
+        {
+            anim.SetBool( "isWalk", false );
+        }
+
+        if ( Input.GetKey( "i" ) )
+        {
+            anim.SetBool( "isWalk", true );
+            verticalAxis += -1;
+        }
+        else if ( Input.GetKeyUp( "i" ) )
+        {
+            anim.SetBool( "isWalk", false );
+        }
+
+        if ( Input.GetKey( "u" ) )
+        {
+            float x = transform.position.x;
+            float y = transform.position.y;
+            float z = transform.position.z;
             if ( canPlaceBomb )
             {
+                anim.SetBool( "attack", true );
                 canPlaceBomb = false;
                 t = ( GameObject ) Instantiate( cube, new Vector3( x, -0.5f, z ), transform.rotation );
             }
-		}
+        }
+        else if ( Input.GetKeyUp( "u" ) )
+        {
+            anim.SetBool( "attack", false );
+        }
 
         if ( Input.GetKey( "o" ) )
         {
             canPlaceBomb = true;
+        }
+        else if ( Input.GetKeyUp( "o" ) )
+        {
+            anim.SetBool( "attack", false );
         }
 
         //TurnInputValue = Input.GetAxis (TurnAxisName);
@@ -91,27 +126,27 @@ public class Player2Movement : MonoBehaviour {
 		{
 			GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
 		}*/
-        Move (horizontalAxis, verticalAxis);
-		//Turning ();
-		//Animating (h, v);
-	}
-	
-	/*void FixedUpdate()
+        Move( horizontalAxis, verticalAxis );
+        //Turning ();
+        //Animating (h, v);
+    }
+
+    /*void FixedUpdate()
 	{
 		Move (horizontalAxis,verticalAxis);
 		//Turning ();
 	}*/
-	
-	void Move(float h, float v)
-	{
-		movement.Set (h, 0f, v);
-		
-		movement = movement.normalized * speed * Time.deltaTime;
-		
-		playerRigidBody.MovePosition (transform.position + movement);
-	}
-	
-	/*void Turning()
+
+    void Move( float h, float v )
+    {
+        movement.Set( h, 0f, v );
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidBody.MovePosition( transform.position + movement );
+    }
+
+    /*void Turning()
 	{
 		// Determine the number of degrees to be turned based on the input, speed and time between frames.
 		float turn = TurnInputValue * TurnSpeed * Time.deltaTime;
@@ -122,10 +157,10 @@ public class Player2Movement : MonoBehaviour {
 		// Apply this rotation to the rigidbody's rotation.
 		playerRigidBody.MoveRotation (playerRigidBody.rotation * turnRotation);
 	}*/
-	
-	void Animating(float h, float v)
-	{
-		bool walking = h != 0f || v != 0f;
-		anim.SetBool ("IsWalking", walking);
-	}
+
+    void Animating( float h, float v )
+    {
+        bool walking = h != 0f || v != 0f;
+        anim.SetBool( "IsWalking", walking );
+    }
 }
